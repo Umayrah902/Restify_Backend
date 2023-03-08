802cf190ee77bac6fd446e3ef2735aa9c1b25354
Umayrah Chonee <umayrah.chonee@mail.utoronto.ca>
2023-03-07 20:34:05 -0500

    user registration and token_auth done

--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializer import UserSerializer
 
-# Create your views here.
+
+class UserSignupView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+# class UserEditProfileView(generics.RetrieveUpdateAPIView):
+#     serializer_class = UserSerializer
+#     permission_classes = (IsAuthenticated,)
+
+#     def get_object(self):
+#         return self.request.user
