6663678e567b22f1c3008a566ef93c1d72cd64c3
NULL <NULL>
2023-03-13 12:47:44 -0400

    Revert "Revert "mass commit""
    This reverts commit d7c747a5fc7d44989c6eb6e929afc332a1bbabb5.

---
+++ properties/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import *
+
+app_name = "properties"
+urlpatterns = [
+    path('', PropertyInfoFetchView.as_view(), name='property_list_view'),
+    path('<int:pk>/', PropertyInfoFocusView.as_view(), name='property_detail_view'),
+    path('<int:pk>/aux/', PropertyAuxMediaView.as_view(), name='property_detail_auxiliary_view'),
+    path('<int:pk>/aux/manage/', PropertyAuxMediaManageView.as_view(), name='property_detail_auxiliary_mgmt_view'),
+    path('<int:pk>/prices/', PropertyPricesView.as_view(), name='property_detail_prices_view'),
+    path('<int:pk>/prices/manage/', PropertyPricesManageView.as_view(), name='property_detail_prices_mgmt_view'),
+    path('create/', PropertyCreateView.as_view(), name='property_detail_view'),
+    path('<int:pk>/edit/', PropertyEditView.as_view(), name='property_detail_view'),
+    path('<int:pk>/bookings/', PropertyBookingsView.as_view(), name='property_bookings_view'),
+    path('<int:pk>/book/', PropertyBookView.as_view(), name='property_book_view'),
+    path('<int:pk>/reviews/', PropertyReviewsView.as_view(), name='property_reviews_view'),
+    path('<int:pk>/review/', PropertyReviewAddView.as_view(), name='property_add_review_view'),
+]
