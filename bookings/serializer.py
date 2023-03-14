c164050097c5e7a4d4320a927160da6b403c116e
NULL <NULL>
2023-03-14 00:15:05 -0400

    Add booking functionality

--- bookings/serializer.py
+++ bookings/serializer.py
@@ -3,11 +3,15 @@ from rest_framework import serializers
 
 
 class BookingSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Booking
-        fields = ('a', 'b')
+        fields = ('pk', 'client', 'property_booking', 'billing_address_string', 'billing_address_city',
+                  'billing_address_country', 'billing_address_province', 'billing_address_postal_code',
+                  'start_date', 'end_date', 'invoice_cost', 'state', )
 
     def create(self, validated_data):
-        booking = Booking.objects.create()
+        booking = BookingSerializer(**validated_data)
         return booking
+
+    def update(self, instance, validated_data):
+        return super().update(instance, validated_data)
--- bookings/views.py
+++ bookings/views.py
@@ -1,19 +1,66 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.status import *
+
+from .models import Booking
 from .serializer import BookingSerializer
 # Create your views here.
 class BookingsDetailView(APIView):
-    def get(self, request):
-        return None
+    permission_classes = [IsAuthenticated]
+    def get(self, request, pk):
+        booking = Booking.objects.get(pk=pk)
+        if booking.property_booking.owner != self.request.user and booking.client != self.request.user:
+            return Response(status=HTTP_401_UNAUTHORIZED)
+        if booking:
+            serializer = BookingSerializer(booking)
+            return Response(serializer.data, status=HTTP_200_OK)
+        else:
+            return Response(status=HTTP_404_NOT_FOUND)
 
 
 class BookingEditView(APIView):
-    def post(self, request):
-        return None
+    def post(self, request, pk):
+        currbooking = Booking.objects.get(pk=pk)
+        if currbooking.property_booking.owner != self.request.user and currbooking.client != self.request.user:
+            return Response(status=HTTP_401_UNAUTHORIZED)
+        try:
+            start_date = request.data['start_date']
+            end_date = request.data['end_date']
+            billing_address_string = request.data['billing_address_string']
+            billing_address_city = request.data['billing_address_city']
+            billing_address_country = request.data['billing_address_country']
+            billing_address_province = request.data['billing_address_province']
+            billing_address_postal_code = request.data['billing_address_postal_code']
+            invoice_cost = request.data['invoice_cost']
+            state = request.data['state']
+            # print(dict(Booking.STATES_OPTIONS))
+            # print(state not in dict(Booking.STATES_OPTIONS))
+
+            if state not in dict(Booking.STATES_OPTIONS):
+                return Response("Bad State", status=HTTP_400_BAD_REQUEST)
+
+            currbooking.start_date = start_date
+            currbooking.end_date = end_date
+            currbooking.billing_address_string = billing_address_string
+            currbooking.billing_address_city = billing_address_city
+            currbooking.billing_address_country = billing_address_country
+            currbooking.billing_address_province = billing_address_province
+            currbooking.billing_address_postal_code = billing_address_postal_code
+            currbooking.invoice_cost = invoice_cost
+            currbooking.state = state
+            currbooking.save()
+
+            serializer = BookingSerializer(currbooking)
+            return Response(serializer.data, status=HTTP_200_OK)
+
+        except KeyError:
+            return Response("Missing Fields in Request", status=HTTP_400_BAD_REQUEST)
 
 
+"""
+"""
 class BookingContactInfoView(APIView):
     def get(self, request):
         return None
