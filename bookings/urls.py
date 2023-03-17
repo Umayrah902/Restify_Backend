2273b15a437274e0ed59f0e4276194319b5f38ce
NULL <NULL>
2023-03-16 23:14:41 -0400

    Revert "Please verify all requirements accomplished."
    This reverts commit 3e823bb0ffa3bdee68a0c547ff69fa21e9c99044.

--- bookings/urls.py
+++ bookings/urls.py
@@ -3,7 +3,6 @@ from .views import *
 
 app_name = "bookings"
 urlpatterns = [
-    path('', BookingsView.as_view(), name='bookings_view'),
     path('<int:pk>/', BookingsDetailView.as_view(), name='bookings_detail_view'),
     path('<int:pk>/edit/', BookingEditView.as_view(), name='property_detail_view'),
     # path('<int:pk>/aux/', BookingContactInfoView.as_view(), name='booking_contact_view'),
