1f94a857990fe6631a2ae1c2651cc2e7e26b26bb
Umayrah Chonee <75751783+Umayrah902@users.noreply.github.com>
2023-04-20 21:30:19 -0400

    Update views.py

--- bookings/views.py
+++ bookings/views.py
@@ -78,7 +78,7 @@ class BookingEditView(APIView):
                     return Response("Bad State", status=HTTP_400_BAD_REQUEST)
 
                 if request.user == currbooking.client:
-                    if state != dict(Booking.STATES_OPTIONS)['RequestCancel']:
+                    if not(state == dict(Booking.STATES_OPTIONS)['RequestCancel'] or (state == "Cancelled" and currbooking.state == "Pending")):
                         return Response("A guest can only request cancellation.", status=HTTP_401_UNAUTHORIZED)
 
                 currbooking.start_date = start_date
